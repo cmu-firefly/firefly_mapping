19757057bffb7f56feb1bc73d11c6d24673f21c8
Kevin Gmelin <kgmelin@andrew.cmu.edu>
2022-03-23 01:49:32 -0400

    Modified mapping_test.py to map an arbitrary png file at a pose specified by a json file. Added example test images and test poses.

--- scripts/mapping_test.py
+++ scripts/mapping_test.py
@@ -1,50 +1,42 @@
 import rospy
 from firefly_mapping.msg import ImageWithPose
 from sensor_msgs.msg import Image
+import json
+from PIL import Image as PILImage
+from PIL import ImageOps
+import rospkg
+import numpy as np
 
 if __name__ == '__main__':
     rospy.init_node('projection_tester', anonymous=True)
     pub = rospy.Publisher('image_to_project', ImageWithPose, queue_size=10)
     pub1 = rospy.Publisher('image', Image, queue_size=10)
 
-    # msg = ImageWithPose()
-    # msg.pose.position.x = 0.151
-    # msg.pose.position.y = -0.011
-    # msg.pose.position.z = 9.865
-    # msg.pose.orientation.x = -0.587
-    # msg.pose.orientation.y = 0.617
-    # msg.pose.orientation.z = -0.381
-    # msg.pose.orientation.w = 0.359
-    # msg.image.width = 640
-    # msg.image.height = 480
-    # msg.image.data = [0] * (640*480)
-    msg = ImageWithPose()
-    msg.pose.position.x = 0.151
-    msg.pose.position.y = -0.011
-    msg.pose.position.z = 29.865
-    msg.pose.orientation.x = -0.689
-    msg.pose.orientation.y = 0.725
-    msg.pose.orientation.z = -0.001
-    msg.pose.orientation.w = -0.002
-    msg.image.width = 200
-    msg.image.height = 150
-    msg.image.data = [0] * (200*150)
-    # msg = ImageWithPose()
-    # msg.pose.position.x = 0.151
-    # msg.pose.position.y = -0.011
-    # msg.pose.position.z = 29.865
-    # msg.pose.orientation.x = -0.683
-    # msg.pose.orientation.y = 0.719
-    # msg.pose.orientation.z = -0.096
-    # msg.pose.orientation.w = 0.088
-    # msg.image.width = 640
-    # msg.image.height = 480
-    # msg.image.data = [0] * (640*480)
+    rospack = rospkg.RosPack()
+    package_path = rospack.get_path('firefly_mapping')
 
-    for i in range(50, 100):
-        for j in range(75, 125):
-            msg.image.data[i*200 + j] = 255
-    msg.image.encoding = 'mono8'
+    im_path = package_path + "/test_data/test_imgs/" + rospy.get_param('~image', "square50.png")
+    pose_path = package_path + "/test_data/test_poses/" + rospy.get_param('~pose', "down_at_30m.json")
+
+    msg = ImageWithPose()
+
+    with open(pose_path) as json_file:
+        data = json.load(json_file)
+        msg.pose.position.x = data['x']
+        msg.pose.position.y = data['y']
+        msg.pose.position.z = data['z']
+        msg.pose.orientation.x = data['qx']
+        msg.pose.orientation.y = data['qy']
+        msg.pose.orientation.z = data['qz']
+        msg.pose.orientation.w = data['qw']
+
+    with PILImage.open(im_path) as im:
+        msg.image.width = im.width
+        msg.image.height = im.height
+        msg.image.data = list(np.array(ImageOps.grayscale(im)).reshape(-1))
+        msg.image.encoding = 'mono8'
+
+
     rospy.sleep(1.0)
     pub.publish(msg)
     pub1.publish(msg.image)
